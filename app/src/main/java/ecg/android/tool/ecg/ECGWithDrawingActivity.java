d03f8bc79cb88d818fb8f269079e517264164537
a0af <a0af@yandex.com>
2017-06-16 12:07:58 +0800

    update

--- app/src/main/java/ecg/android/tool/ecg/ECGWithDrawingActivity.java
+++ app/src/main/java/ecg/android/tool/ecg/ECGWithDrawingActivity.java
@@ -48,7 +48,7 @@ import ecg.android.tool.profile.BleProfileActivity;
 import no.nordicsemi.android.nrftoolbox.R;
 import no.nordicsemi.android.nrftoolbox.utility.DebugLogger;
 
-public class ECGActivity extends BleProfileActivity implements ECGManagerCallbacks, OnChartValueSelectedListener {
+public class ECGWithDrawingActivity extends BleProfileActivity implements ECGManagerCallbacks, OnChartValueSelectedListener {
 
     private final static String GRAPH_STATUS = "graph_status";
     private final static String HR_VALUE = "hr_value";
@@ -305,100 +305,6 @@ public class ECGActivity extends BleProfileActivity implements ECGManagerCallbac
 
         YAxis rightAxis = mChart.getAxisRight();
         rightAxis.setEnabled(false);
-//        setData(1000,2000);
-    }
-
-    private void setData(int count, float range) {
-
-        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
-
-        for (int i = 0; i < count; i++) {
-            float mult = range / 2f;
-            float val = (float) (Math.random() * mult) + 50;
-            yVals1.add(new Entry(i, val));
-        }
-
-        ArrayList<Entry> yVals2 = new ArrayList<Entry>();
-
-        for (int i = 0; i < count - 1; i++) {
-            float mult = range;
-            float val = (float) (Math.random() * mult) + 450;
-            yVals2.add(new Entry(i, val));
-//            if(i == 10) {
-//                yVals2.add(new Entry(i, val + 50));
-//            }
-        }
-
-        ArrayList<Entry> yVals3 = new ArrayList<Entry>();
-
-        for (int i = 0; i < count; i++) {
-            float mult = range;
-            float val = (float) (Math.random() * mult) + 500;
-            yVals3.add(new Entry(i, val));
-        }
-
-        LineDataSet set1, set2, set3;
-
-        if (mChart.getData() != null &&
-                mChart.getData().getDataSetCount() > 0) {
-            set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
-            set2 = (LineDataSet) mChart.getData().getDataSetByIndex(1);
-            set3 = (LineDataSet) mChart.getData().getDataSetByIndex(2);
-            set1.setValues(yVals1);
-            set2.setValues(yVals2);
-            set3.setValues(yVals3);
-            mChart.getData().notifyDataChanged();
-            mChart.notifyDataSetChanged();
-        } else {
-            // create a dataset and give it a type
-            set1 = new LineDataSet(yVals1, "DataSet 1");
-
-            set1.setAxisDependency(YAxis.AxisDependency.LEFT);
-            set1.setColor(ColorTemplate.getHoloBlue());
-            set1.setCircleColor(Color.WHITE);
-            set1.setLineWidth(2f);
-            set1.setCircleRadius(1f);
-            set1.setFillAlpha(65);
-            set1.setFillColor(ColorTemplate.getHoloBlue());
-            set1.setHighLightColor(Color.rgb(244, 117, 117));
-            set1.setDrawCircleHole(false);
-            //set1.setFillFormatter(new MyFillFormatter(0f));
-            //set1.setDrawHorizontalHighlightIndicator(false);
-            //set1.setVisible(false);
-            //set1.setCircleHoleColor(Color.WHITE);
-
-            // create a dataset and give it a type
-            set2 = new LineDataSet(yVals2, "DataSet 2");
-            set2.setAxisDependency(YAxis.AxisDependency.RIGHT);
-            set2.setColor(Color.RED);
-            set2.setCircleColor(Color.WHITE);
-            set2.setLineWidth(2f);
-            set2.setCircleRadius(2f);
-            set2.setFillAlpha(65);
-            set2.setFillColor(Color.RED);
-            set2.setDrawCircleHole(false);
-            set2.setHighLightColor(Color.rgb(244, 117, 117));
-            //set2.setFillFormatter(new MyFillFormatter(900f));
-
-            set3 = new LineDataSet(yVals3, "DataSet 3");
-            set3.setAxisDependency(YAxis.AxisDependency.RIGHT);
-            set3.setColor(Color.YELLOW);
-            set3.setCircleColor(Color.WHITE);
-            set3.setLineWidth(2f);
-            set3.setCircleRadius(3f);
-            set3.setFillAlpha(65);
-            set3.setFillColor(ColorTemplate.colorWithAlpha(Color.YELLOW, 200));
-            set3.setDrawCircleHole(false);
-            set3.setHighLightColor(Color.rgb(244, 117, 117));
-
-            // create a data object with the datasets
-            LineData data = new LineData(set1, set2, set3);
-            data.setValueTextColor(Color.WHITE);
-            data.setValueTextSize(9f);
-
-            // set data
-            mChart.setData(data);
-        }
     }
 
     //    ---------------BEGIN Calculate Average Ecg Count ---------------------------------
@@ -439,7 +345,7 @@ public class ECGActivity extends BleProfileActivity implements ECGManagerCallbac
     }
     //    ----------------END Calculate Average Fetch ( Drawing ) Count --------------------------------
 
-    int fetchCount = 0;
+    int fetchCount =1;
     Handler timerHandler;
 
     final Runnable runnableFroDraw = new Runnable() {
@@ -513,13 +419,13 @@ public class ECGActivity extends BleProfileActivity implements ECGManagerCallbac
 
             //
 //            if (fetchCount == 0) return;
-            for (int i = 0; i < 45; i++) {
+            for (int i = 0; i < 55; i++) {
                 if (isEmptyQueue()) break;
                 int val = deQueue();
                 if (val == -1) break;
 
-                data.addEntry(new Entry(nXBufferSize, val), 0);
-//                set.addEntry(new Entry(nXBufferSize, val));
+//                data.addEntry(new Entry(nXBufferSize, val), 0);
+                set.addEntry(new Entry(nXBufferSize, val));
 //                setX.addEntry(new Entry(nXBufferSize, val.x));
 //                setY.addEntry(new Entry(nXBufferSize, val.y));
 //                setZ.addEntry(new Entry(nXBufferSize, val.z));
@@ -536,7 +442,7 @@ public class ECGActivity extends BleProfileActivity implements ECGManagerCallbac
             }
 
             // n page show
-            if (set.getEntryCount() > X_LENGTH * 300) {
+            if (set.getEntryCount() > X_LENGTH * 50) {
                 for (int j = 0; j < 100; j++) {
                     set.removeFirst();
 //                    setX.removeFirst();
